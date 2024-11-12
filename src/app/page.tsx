3ea61cf07101b90fe508bc75595eb33cbf3b483e
Will Sather <will@sather.ws>
2024-11-12 18:01:59 -0500

    fix: text sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import LogoIcon from "@/icons/logo";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center bg-green-50 p-24">
+    <main className="flex min-h-screen flex-col items-center bg-green-50 p-8 md:p-24">
       <LogoIcon width={100} height={200} />
 
-      <h1 className="my-8 font-bold text-green-800">MN Civic Tech</h1>
+      <h1 className="my-8 text-center font-bold text-green-800">
+        MN Civic Tech
+      </h1>
 
       <p className="text-green-800">🚧 coming soon 🚧</p>
     </main>
