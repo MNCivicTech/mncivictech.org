6d13c0061ec2f0770997d0e222db2d0506336633
Will Sather <will@sather.ws>
2024-11-12 17:37:36 -0500

    fix: background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center bg-green-50 p-24">
+    <main className="flex min-h-screen flex-col items-center bg-green-50 p-24">
       <h1 className="my-8 font-bold">MN Civic Tech</h1>
 
       <p>coming soon ...</p>
